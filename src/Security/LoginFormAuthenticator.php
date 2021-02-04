184da1f7da8f30baf68b36695bd6e02c6bcd8d2c
Andrei Curta <curta.andrei98@gmail.com>
2021-02-05 00:15:37 +0200

    adaugat autentificare

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -90,7 +90,7 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator
             return new RedirectResponse($targetPath);
         }
 
-        return new RedirectResponse($this->urlGenerator->generate('/main'));
+        return new RedirectResponse($this->urlGenerator->generate('main'));
 
     }
 
